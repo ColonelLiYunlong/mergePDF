0bf6009be434535e873c4b2dfe85d4857d820d25
ColonelLiYunlong <38041054+ColonelLiYunlong@users.noreply.github.com>
2019-07-26 17:11:50 +0800

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+from PyPDF2 import PdfFileReader, PdfFileWriter
+def merge_odd_even(odd, even, outfn):
+    pdf_output = PdfFileWriter()
+    odd_input = PdfFileReader(open(odd,'rb'))
+    even_input = PdfFileReader(open(even,'rb'))
+    odd_page = odd_input.getNumPages()
+    even_page = even_input.getNumPages()
+    if odd_page == even_page:
+        print('The number of pages is {}'.format(odd_page*2))
+        for i in range(odd_page):
+            pdf_output.addPage(odd_input.getPage(i))
+            pdf_output.addPage(even_input.getPage(-(i+1)))
+    else:
+        print('Check page number')
+    pdf_output.write(open(outfn, 'wb'))
+
+if __name__ == '__main__':
+    odd = 'odd.pdf'
+    even = 'even.pdf'
+    filename = input('Input file name:')
+    outfn = filename + '.pdf'
+    merge_odd_even(odd,even,outfn)
