83052b7237a219d3efdb9c5fbfae0e302d0d2efb
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-06-20 17:38:31 -0500

    Style: Add docstrings

--- merge.py
+++ merge.py
@@ -2,7 +2,19 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
-def merge_odd_even(odd, even, output_filename):
+def merge_odd_even(odd, even, output_file):
+    """
+    Return a merged pdf from two pdfs.
+
+    Parameters:
+        odd:
+            - pdf filename with odd pages in descending order.
+        even:
+            - pdf filename with even pages in descending order.
+    Returns:
+        output_file:
+            - pdf filename with merged pdf.
+    """
     pdf_output = PdfFileWriter()
     odd_input = PdfFileReader(open(odd, 'rb'))
     even_input = PdfFileReader(open(even, 'rb'))
@@ -14,7 +26,8 @@ def merge_odd_even(odd, even, output_filename):
         for i in range(odd_pages):
             pdf_output.addPage(odd_input.getPage(i))
             pdf_output.addPage(even_input.getPage(i))
-        pdf_output.write(open(output_filename, 'wb'))
+        pdf_output.write(open(output_file, 'wb'))
+        print('Documents merged successfully.')
     else:
         print('Check page number: Odd pages unequal to even pages')
 
@@ -22,6 +35,7 @@ def merge_odd_even(odd, even, output_filename):
 if __name__ == '__main__':
     odd = 'odd.pdf'
     even = 'even.pdf'
-    filename = 'merged.pdf'
-    output_filename = filename
-    merge_odd_even(odd, even, output_filename)
+    filename = input(
+        'Input the file name for the merged document (without .pdf): ')
+    output_file = filename + '.pdf'
+    merge_odd_even(odd, even, output_file)
